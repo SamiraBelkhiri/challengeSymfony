0b4545127c79566b1cdfd8c6208aea10b042a728
SamiraBelkhiri <belkhirisamira@hotmail.com>
2020-03-03 16:18:23 +0100

    made reset and added changes, one controller for 3 pages

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -8,18 +8,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class LearningController extends AbstractController
 {
     /**
-     * @Route("/about-me", name="learning")
+     * @Route("/learning", name="learning")
      */
     public function index()
     {
         return $this->render('learning/index.html.twig', [
             'controller_name' => 'Samira',
-            'coach' => $this->aboutMe(),
+            'coach' => 'sicco',
+        ]);
+
+    }
+
+
+    /**
+     * @Route("/change/name", name="change_name")
+     */
+    public function changeName()
+    {
+        return $this->render('change_name/index.html.twig', [
+            'controller_name' => 'ChangeNameController',
+            'name' => 'SAMMYB',
         ]);
     }
 
-    public function aboutMe()
+    /**
+     * @Route("/show/name", name="show_name")
+     */
+    public function showName()
     {
-        return 'loremipsum';
+        return $this->render('show_name/index.html.twig', [
+            'controller_name' => 'showNameController',
+            'name' => 'SAMMYB',
+        ]);
     }
 }
